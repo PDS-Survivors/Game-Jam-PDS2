bc9587d1ba723c6b08a493b4a204214a44dbb5fe
LeoAGS <leoagdasilva@gmail.com>
2022-10-20 15:02:30 -0300

    test

---
+++ include/building.hpp
@@ -0,0 +1,35 @@
+#ifndef BUILDING_HPP
+#define BUILDING_HPP
+
+#include <iostream>
+#include <vector>
+#include <stack>
+
+class Building {
+
+    private:
+    //std::stack<Battle*> _battles;
+    std::string _desc;
+    std::string _name;
+    int _number;
+
+    public:
+
+
+    std::string get_desc();
+
+    std::string get_name();
+
+    int get_number();
+
+    void start_battle();
+
+    void add_battle();
+    
+    void remove_battle();
+
+    //bool play_battles(Battle* battle);
+
+};
+
+#endif
