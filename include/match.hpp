85e7a50b519d7ddc4201c55ba4e133b347b89db3
Beatriz-SiC <beatrizscampagnaro@gmail.com>
2022-11-19 19:26:27 -0300

    Classe Match
    Arquivos criados

---
+++ include/match.hpp
@@ -0,0 +1,39 @@
+#ifndef MATCH_HPP
+#define MATCH_HPP
+
+
+
+#include "building.hpp"
+#include "effects/effect.hpp"
+#include "entities/pc.hpp"
+#include <string>
+#include <vector>
+#include <stack>
+
+class Match {
+    private:
+        Pc* _player;
+        std::stack<Building*> _buildings;
+        std::vector<Effect*> _permanentEffects;
+        int _numLifes;
+        std::string _epilogue;
+
+    public:
+        //construtor
+        Match();
+
+        void playBuilding(Building*);
+
+        int chooseEffect();
+
+        void eventHappen(int);
+
+        //errado, essa função tem que receber alguma coisa, estava em duvida do que seria especificamente por isso não pus
+        void setEpilogue();
+        
+        void printEpilogue();
+
+
+};
+
+#endif
