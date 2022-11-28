b3c0355e140b652c3a8e6bd5b7aed6d6d534040f
LeoAGS <leoagdasilva@gmail.com>
2022-11-28 12:21:30 -0300

    Update building.hpp

--- include/building.hpp
+++ include/building.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file building.hpp
+ * @author Leonardo Antonio Gomes da Silva (Leoagdasilva@gmail.com)
+ * @brief 
+ * @details 
+ * @version 1.0
+ * @date 2022-11-28
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 #ifndef BUILDING_HPP
 #define BUILDING_HPP
 
@@ -8,76 +19,100 @@
 #include <stack>
 #include <string>
 
+
+/**
+ * @brief Em qual predio da faculdade o PC esta
+ * @details Classe responsavel pela classe "building" que se refere al predio da universidade em que o
+ * player "pc" esta, responsavel tambem pela criação da pilha de batalhas e tambem pela inicialização
+ * das batalhas 
+ * 
+ */
 class Building {
 
     private:
-    std::stack<Battle*> _battles;
-    std::string _desc;
-    std::string _name;
-    int _number;
-    int _numBattle;
-    int _totalBattles;
-    bool _tryagain;
+
+        std::stack<Battle*> _battles;
+        std::string _desc;
+        std::string _name;
+        int _number;
+        int _numBattle;
+        int _totalBattles;
+        bool _tryagain;
 
     public:
-    /**
-     * @brief Construct a new Building object
-     * @param desc Complete descripition of the building
-     * @param name Name of the building
-     * @param number Number of the building
-     * @param totalb Number of battles in the building
-    */
-    Building(std::string desc, std::string name, int number, int totalb);
 
-    ~Building();
+        /**
+        * @brief Constructor a new Building object
+        * @details Construtor de um objeto building a partir de parametros
+        * @param desc Complete descripition of the building
+        * @param name Name of the building
+        * @param number Number of the building
+        * @param totalb Number of battles in the building
+        */
+        Building(std::string desc, std::string name, int number, int totalb);
 
-    /**
-     * @brief Return the descripition of the building 
-    */
-    std::string get_desc();
+        /**
+         * @brief Destructor of a building object
+         * @details Destroi o objeto building 
+         * 
+         */
+        ~Building();
 
-    /**
-     * @brief Return the name of the building
-    */
-    std::string get_name();
+        /**
+         * @brief Retorna a descrição de building 
+         * @details Essa função retorna a descrição completa do objeto builidng, predio, em que
+         *  o player esta.
+        */
+        std::string get_desc();
 
-    /**
-     * @brief Return the number of the building
-    */
-    int get_number();
+        /**
+         * @brief Retorna o nome de building 
+         * @details Essa função retorna o nome do objeto builidng, predio, em que o player esta.
+        */
+        std::string get_name();
 
-    /**
-     * @brief Return the nuber of the battle
-    */
-    int get_numBattle();
+        /**
+         * @brief Retorna o numero de building 
+         * @details Essa função retorna o numero referente ao objeto builidng, predio, em que
+         *  o player esta.
+        */
+        int get_number();
 
-    /**
-     * @brief Set the nuber of the battle
-    */
-    void set_numBattle(int num);
+        /**
+         * @brief Return the nuber of the battle
+         * @details
+        */
+        int get_numBattle();
 
-    /**
-     * @brief Starts a battle
-    */
-    void start_battle();
+        /**
+         * @brief Set the nuber of the battle
+         * @details
+        */
+        void set_numBattle(int num);
 
-    /**
-     * @brief Adds a battle on the stack
-    */
-    void add_battles();
+        /**
+         * @brief Starts a battle
+         * @details
+        */
+        void start_battle();
 
-    /**
-     * @brief Removes a battle from the stack
-    */
-    void remove_battle();
+        /**
+         * @brief 
+         * @details
+        */
+        void doBattle();
 
-    //bool play_battles(Battle* battle);
+        /**
+         * @brief 
+         * @details
+        */
+        bool isComplete();
 
-    void doBattle();
-
-    bool isComplete();
-
-    void manageAttacks();
+        /**
+         * @brief 
+         * @details
+        */
+        void manageAttacks();
 
 };
 
