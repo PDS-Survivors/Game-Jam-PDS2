64681cf7718754284688d4e14843f182f62b7111
camsa1908 <112005242+camsa1908@users.noreply.github.com>
2022-12-05 18:06:37 -0300

    Update read_test.cpp

--- tests/read_test.cpp
+++ tests/read_test.cpp
@@ -4,42 +4,42 @@
 
 TEST_CASE("Teste de unidade - readfile")
 {
-    SUBCASE("Caso positivo: Leitura de arquivo com êxito")
+    SUBCASE("Caso Leitura de arquivo com êxito")
     {
         std::vector<int> v; std::vector<std::string> w; std::vector<std::string> f;
-        CHECK_NOTHROW(read::readfile("../lib/npcs/lelaq.txt", v, w, f));
+        CHECK_NOTHROW(reader::readfile("../lib/npcs/lelaq.txt", v, w, f));
     }
-    SUBCASE("Caso negativo: Leitura de arquivo inexistente")
+    SUBCASE("Caso Leitura de arquivo inexistente")
     {
         std::vector<int> v; std::vector<std::string> w; std::vector<std::string> f;
-        CHECK_THROWS(read::readfile("../lib/npcs/naoexiste.txt", v, w, f));
+        CHECK_THROWS(reader::readfile("../lib/npcs/naoexiste.txt", v, w, f));
     }
-    SUBCASE("Caso negativo: Leitura de arquivo com formato não suportado")
+    SUBCASE("Caso Leitura de arquivo com formato não suportado")
     {
         std::vector<int> v; std::vector<std::string> w; std::vector<std::string> f;
-        CHECK_NOTHROW(read::readfile("../tests/teste.txt", v, w, f));
+        CHECK_NOTHROW(reader::readfile("../tests/teste.txt", v, w, f));
     }
 }
 
 TEST_CASE("Teste de unidade - readint")
 {
-    SUBCASE("Caso postivo: numero positivo")
+    SUBCASE("Caso numero positivo")
     {
         std::string::iterator it;
         std::string line = "01_";
-        CHECK_EQ(10, read::readint(it, line));
+        CHECK(reader::readint(it, line) == 10);
     }
-    SUBCASE("Caso especial: numero negativo")
+    SUBCASE("Caso especial numero negativo")
     {
         std::string::iterator it;
         std::string line = "01-_";
-        CHECK_EQ(-10, read::readint(it, line));
+        CHECK(reader::readint(it, line) == -10);
     }
-    SUBCASE("Caso negativo: formato errado")
+    SUBCASE("Caso formato errado")
     {
         std::string::iterator it;
         std::string line = "10_";
-        CHECK_EQ(10, read::readint(it, line));
+        CHECK(reader::readint(it, line) == 1);
     }
 }
 
@@ -49,12 +49,12 @@ TEST_CASE("Teste de unidade - readstring")
     {
         std::string::iterator it;
         std::string line = "string de teste_";
-        CHECK_EQ("string de teste", read::readint(it, line));
+        CHECK_EQ("string de teste", reader::readint(it, line));
     }
     SUBCASE("Caso negativo")
     {
         std::string::iterator it;
         std::string line = "string de teste";
-        CHECK_EQ("string de teste", read::readint(it, line));
+        CHECK(reader::readint(it, line));
     }
 }
